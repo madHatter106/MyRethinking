9c242dced2e7cf17ba21acdca7680039eafa1b72
madHatter106 <jkomd.283.2112@gmail.com>
2019-02-22 22:36:38 -0500

    completed haunted dag

--- ch6_3.py
+++ ch6_3.py
@@ -47,11 +47,13 @@ b_PC = 1 # direct effect of P on C
 b_U = 2 # direct effect of U on P and C
 
 np.random.seed(1)
-U = np.random.binomial(n=1, p=0.5, size=n)
+U = 2* np.random.binomial(n=1, p=0.5, size=n)-1
 G = np.random.normal(size=n)
 P = np.random.normal(loc=b_GP*G + b_U*U)
 C = np.random.normal(b_PC*P + b_GC*G + b_U*U)
 d_ = pd.DataFrame(dict(C=C, P=P, G=G, U=U))
+d_.head()
+d_.U.value_counts()
 dcorr = d_.corr()
 dcorr.head()
 _, ax = pl.subplots()
@@ -72,3 +74,26 @@ pm.model_to_graphviz(m11)
 with m11:
     trc11 = pm.sample(1000, tune=1500)
 pm.summary(trc11, alpha=0.11)
+"""
+The unmeasured U makes P a collider, and conditioning on P produces collider
+bias. Assuming we can measure U, below is the multivariate regression that also
+conditions on U:
+"""
+with pm.Model() as m12:
+    σ = pm.Exponential('σ', 1)
+    β_PC = pm.Normal('β_PC', 0, 1)
+    β_GC = pm.Normal('β_GC', 0, 1)
+    β_U = pm.Normal('β_U', 0, 1)
+    α = pm.Normal('α', 0, 1)
+    μ = α + β_PC * d_.P.values + β_GC * d_.G.values + β_U * d_.U.values
+    C = pm.Normal('C', mu=μ, sd=σ, observed=d_.C.values)
+    trc_12 = pm.sample(1000, tune=1500)
+
+pm.summary(trc_12, alpha=0.11)
+"""
+The above is an example of Simpson's Paradox. Controlling for one predictor,
+P in this case, can reverse the direction of association between some other
+predictor (G) and the outcome (C).
+Usually Simpson's paradox is presented in cases where adding the new predictor
+helps us. The opposite happens here. 
+"""
