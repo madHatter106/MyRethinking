corrected path and tested manual waic computation

ch7_4.py
@@ -1,10 +1,16 @@
+import pathlib
+
 import numpy as np
 import pandas as pd
 import pymc3 as pm
 import scipy.stats as stats
 from scipy.special import logsumexp
 
-df = pd.read_csv('./rethinking-Experimental/data/cars.csv')
+pwd
+home = pathlib.Path.home()
+root = home / 'DEV-ALL/BAYES/rethinking_2ndEd/rethinking-Experimental/data'
+df = pd.read_csv(root / 'cars.csv')
+
 df.head()
 with pm.Model() as m:
     α = pm.Normal('α', 0, 100)
@@ -21,8 +27,7 @@ n_samples = 1000
 ll = stats.norm.logpdf(df.dist.values.reshape(-1, 1), loc=μ_post, scale=post['σ'][:n_samples].reshape(1, -1))
 lppd = logsumexp(ll, axis=1) - np.log(n_samples)
 pwaic = np.var(ll, axis=1)
--2 * (lppd.sum() - pwaic.sum())
 lppd2 = np.log(np.sum(np.exp(ll), axis=1)) - np.log(n_samples)
-lppd
--2*(lppd.sum() - pwaic.sum())
--2*(lppd2.sum() - pwaic.sum())
+np.testing.assert_array_almost_equal(lppd2, lppd)
+waic = -2*(lppd.sum() - pwaic.sum())
+waic